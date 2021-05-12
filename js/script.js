561ed9f20d7c4b79511da53df28a0314e24a036e
Dorota Kmiecik <dorota.kmiecik87@gmail.com>
2021-05-13 01:43:19 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -9,23 +9,57 @@
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    };
+
     const render = () => {
         let htmlString = "";
 
-        for (const task of tasks) {
+        for (const work of tasks) {
             htmlString += `
-                <li class="task__element"><button class="task__input"><i class="fa fa-check" aria-hidden="true"></i></button>
-                <p class="task__label" for="scales"> ${task.content} </p>
-                <button class="task__button"><i class="fa fa-trash-o"></i></button></li>
-                
+                <li ${work.done ? "style= \"text-decoration: line-through\"" : ""} class="task__element">
+                <button class="task__input js-done"><i class="fa fa-check" aria-hidden="true"></i></button>
+                <p class="task__label"> ${work.content} </p>
+                <button class="task__button js-remove"><i class="fa fa-trash-o"></i></button></li>        
             `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+        const removeButtons = document.querySelectorAll(".js-remove");
+        removeButtons.forEach((removeButtons, index) => {
+            removeButtons.addEventListener("click", () => {
+                removeTask(index);
+            })
+
+        });
+
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        if (newTaskContent === "") {
+            return;
+        }
+        addNewTask(newTaskContent);
+
     };
 
     const init = () => {
-        render()
+        render();
+
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit)
+
     };
     init();
 }
